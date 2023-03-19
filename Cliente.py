10d4b5cbf80d6ad97c09f679cb4be2b5ab82840a
Ronaldo <ronaldinho.lfs@gmail.com>
2023-03-19 18:31:54 -0300

    Segundo comit

--- Cliente.py
+++ Cliente.py
@@ -1,4 +1,5 @@
 import socket
+import datetime
 from time import sleep
 from datetime import datetime, timedelta
 
@@ -12,6 +13,24 @@ sock.connect((HOST, PORT))
 # Tempo de ajuste
 ADJUST_TIME = 10  # segundos
 
+
+def update_system_time(timestamp):
+    """
+    Atualiza o relógio do sistema com o timestamp Unix fornecido.
+    """
+    # Cria um objeto datetime a partir do timestamp
+    dt = datetime.datetime.fromtimestamp(timestamp)
+
+    # Obtém a data e hora em um formato de string
+    date_str = dt.strftime('%Y-%m-%d')
+    time_str = dt.strftime('%H:%M:%S')
+
+    # Define a data e hora do sistema
+    import os
+    os.system(f'date {date_str}')
+    os.system(f'time {time_str}')
+
+
 while True:
     # Recebe o horário do servidor
     server_time_str = sock.recv(1024).decode()
@@ -23,7 +42,13 @@ while True:
     # Ajusta o relógio gradualmente
     for i in range(ADJUST_TIME):
         new_time = datetime.now() + diff/ADJUST_TIME
+        # Ajusta o relógio local do cliente
+        adjusted_time = time.time() + time_diff
+
+        # Atualiza o relógio do sistema
+        update_system_time(adjusted_time)
         print(f"Horário ajustado para: {new_time}")
         sleep(1)
+    sleep(3)
 
 sock.close()
