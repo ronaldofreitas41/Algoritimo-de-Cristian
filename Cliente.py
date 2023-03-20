5166b6187502a5cb8fa0d9b244cb127d5fb7769c
ronaldofreitas41 <ronaldinho.lfs@gmail.com>
2023-03-20 09:52:50 -0300

    Teste 3 agora vai

--- Cliente.py
+++ Cliente.py
@@ -1,54 +1,67 @@
+import ctypes
 import socket
-import datetime
-from time import sleep
-from datetime import datetime, timedelta
+import time
 
-HOST = '192.168.2.7'  # endereço IP do servidor
-PORT = 8888  # porta em que o servidor está escutando
+# Configuração do cliente
+HOST = 'localhost'
+PORT = 12345
 
-# Configuração do socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
+# Constantes do algoritmo de Christian
+ALPHA = 0.1
+BETA = 0.25
+ADJUSTMENT_INTERVAL = 10
 
-# Tempo de ajuste
-ADJUST_TIME = 10  # segundos
+# Criação do socket do cliente
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
+# Variáveis para o algoritmo de Christian
+offset = 0
+delay = 0
+last_adjustment = 0
 
-def update_system_time(timestamp):
-    """
-    Atualiza o relógio do sistema com o timestamp Unix fornecido.
-    """
-    # Cria um objeto datetime a partir do timestamp
-    dt = datetime.datetime.fromtimestamp(timestamp)
+# Função para ajustar a hora do sistema Linux
+def set_system_time(seconds, microseconds):
+    # Define a estrutura timeval com os valores de tempo a serem definidos
+    timeval = ctypes.Structure(
+        'timeval', 
+        [('tv_sec', ctypes.c_long), ('tv_usec', ctypes.c_long)]
+    )
+    tv = timeval(seconds, microseconds)
 
-    # Obtém a data e hora em um formato de string
-    date_str = dt.strftime('%Y-%m-%d')
-    time_str = dt.strftime('%H:%M:%S')
-
-    # Define a data e hora do sistema
-    import os
-    os.system(f'date {date_str}')
-    os.system(f'time {time_str}')
-
+    # Chama a função settimeofday do sistema Linux
+    libc = ctypes.CDLL('libc.so.6')
+    return libc.settimeofday(ctypes.byref(tv), None)
 
 while True:
     # Recebe o horário do servidor
-    server_time_str = sock.recv(1024).decode()
-    server_time = datetime.strptime(server_time_str, "%a %b %d %H:%M:%S %Y")
+    server_time = float(s.recv(1024).decode())
 
-    # Calcula a diferença de tempo
-    diff = server_time - datetime.now()
+    # Obtém o horário local
+    local_time = time.time()
 
-    # Ajusta o relógio gradualmente
-    for i in range(ADJUST_TIME):
-        new_time = datetime.now() + diff/ADJUST_TIME
-        # Ajusta o relógio local do cliente
-        adjusted_time = time.time() + time_diff
+    # Calcula o delay e o offset
+    delay = (time.time() - local_time) / 2
+    offset = server_time + delay - local_time
 
-        # Atualiza o relógio do sistema
-        update_system_time(adjusted_time)
-        print(f"Horário ajustado para: {new_time}")
-        sleep(1)
-    sleep(3)
+    # Atualiza o horário local gradativamente usando o algoritmo de Christian
+    local_time = local_time + ALPHA * offset
+    delay = (1 - ALPHA) * delay + ALPHA * abs(offset)
 
-sock.close()
+    # Verifica se é hora de ajustar o relógio
+    if time.time() - last_adjustment > ADJUSTMENT_INTERVAL:
+        # Ajusta o horário em 10 segundos
+        local_time += 10
+        last_adjustment = time.time()
+
+        # Obtém os segundos e microssegundos a partir do novo horário local
+        seconds = int(local_time)
+        microseconds = int((local_time - seconds) * 1000000)
+
+        # Chama a função para ajustar a hora do sistema
+        set_system_time(seconds, microseconds)
+
+    # Aguarda um segundo antes de atualizar o horário novamente
+    time.sleep(1)
+
+    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(local_time)))
--- Servidor.py
+++ Servidor.py
@@ -1,35 +1,32 @@
 import socket
 import ntplib
-from time import ctime
+import time
 
-NTP_SERVER = "br.pool.ntp.org"
-HOST = '192.168.2.7'  # endereço IP do servidor
-PORT = 8888  # porta que o servidor estará escutando
+# Configuração do servidor
+HOST = 'localhost'
+PORT = 12345
 
-# Configuração do cliente NTP
+# Conexão com o servidor NTP
 ntp_client = ntplib.NTPClient()
+ntp_server = 'br.pool.ntp.org'
 
-# Configuração do socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-sock.listen(1)
-
-print(f"Servidor de hora iniciado em {HOST}:{PORT}")
+# Criação do socket do servidor
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
 
 while True:
-    conn, addr = sock.accept()
-    print(f"Conexão estabelecida com {addr}")
-    try:
-        # Obtem o horário do servidor NTP
-        response = ntp_client.request(NTP_SERVER)
-        ntp_time = response.tx_time
+    # Aguarda conexão com um cliente
+    conn, addr = s.accept()
+    print('Conectado por', addr)
 
-        # Converte o horário para uma string formatada
-        date_time = ctime(ntp_time)
+    while True:
+        # Obtém o horário do servidor NTP
+        response = ntp_client.request(ntp_server)
+        server_time = response.tx_time
 
         # Envia o horário para o cliente
-        conn.send(date_time.encode())
-    except Exception as e:
-        print(f"Erro ao obter horário do servidor NTP: {e}")
-    finally:
-        conn.close()
+        conn.sendall(str(server_time).encode())
+
+        # Aguarda 5 segundos antes de atualizar o horário novamente
+        time.sleep(5)
